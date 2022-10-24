484f854ea9b1a87cf9c6bb891faad7a28c81d00a
Hiago de Oliveira Mendes <99771272+hiagophd@users.noreply.github.com>
2022-10-24 16:48:42 -0300

    Delete ListaPessoas4 directory

--- ListaPessoas4/src/ListaPessoas/Ordenadores/Ordenador.java
+++
@@ -1,9 +0,0 @@
-package ListaPessoas.Ordenadores;
-
-import ListaPessoas.ColecaoDePessoas;
-
-public interface Ordenador {
-	
-	public void PorAnoDeNascimento(ColecaoDePessoas listaDePessoas, int inicio, int fim);
-	
-}
